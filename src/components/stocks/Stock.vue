3afd369a24ed0e3f5696f37e3bccca6e5845b99a
arthurbz <arthurbziero@gmail.com>
2022-01-14 17:31:16 -0300

    add stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,20 @@
 <template>
-    <h1>Stock</h1>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>
+                    Name
+                    <small>Price</small>
+                </strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantity" type="number" />
+                <v-btn class="green darken-3 white--text">Buy</v-btn>
+            </v-container>
+        </v-card>    
+    </v-flex>
 </template>
 
 <script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-    <h1>Stocks Pane</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" />
+    </v-layout>
 </template>
 
 <script>
-export default {
+import Stock from './Stock'
 
+export default {
+    components: { Stock },
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'TSLA', price: 1040 },
+                { id: 2, name: 'AMZN', price: 3222 },
+                { id: 3, name: 'GOOG', price: 2800 },
+                { id: 4, name: 'IBM', price: 134 },
+            ]
+        }
+    }
 }
 </script>
 
