c0e37cf61ee089379dd33b896d8ed5a209455f5c
arthurbz <arthurbziero@gmail.com>
2022-01-15 16:25:27 -0300

    add portfolio to vuex

---
+++ src/store/modules/portoflio.js
@@ -0,0 +1,54 @@
+export default {
+    state: {
+        funds: 10000,
+        stocks: []
+    },
+    mutations: {
+        buyStock(state, { id, quantity, price }) {
+            const record = state.stocks.find(element => element.id == id)
+
+            if (record) {
+                record.quantity += quantity
+            } else {
+                state.stocks.push({
+                    id: id,
+                    quantity: quantity,
+                })
+            }
+            state.funds -= price * quantity
+        },
+
+        sellStock(state, { id, quantity, price }) {
+            const record = state.stocks.find(element => element.id == id)
+
+            if (record.quantity > quantity) {
+                record.quantity -= quantity
+            } else {
+                state.stocks.splice(state.stocks.indexOf(record), 1)
+            }
+            state.funds += price * quantity
+        }
+    },
+    actions: {
+        sellStock({ commit }, order) {
+            commit('sellStock', order)
+        }
+    },
+    getters: {
+        stockPortfolio(state, geters) {
+            return state.stocks.map(stock => {
+                const record = state.stocks.find(element => element.id == id)
+                return {
+                    id: stock.id,
+                    quantity: stock.quantity,
+                    name: record.name,
+                    price: record.price
+                }
+            })
+        },
+
+        funds(state) {
+            return state.funds
+        }
+    }
+}
