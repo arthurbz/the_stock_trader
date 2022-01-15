ed30d531520f9009cef61196deda5279d5d2fd6c
arthurbz <arthurbziero@gmail.com>
2022-01-15 12:18:18 -0300

    adding Vuex to the project

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -9,14 +9,9 @@ import Stock from './Stock'
 
 export default {
     components: { Stock },
-    data() {
-        return {
-            stocks: [
-                { id: 1, name: 'TSLA', price: 1040 },
-                { id: 2, name: 'AMZN', price: 3222 },
-                { id: 3, name: 'GOOG', price: 2800 },
-                { id: 4, name: 'IBM', price: 134 },
-            ]
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks
         }
     }
 }
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'TSLA', price: 1040 },
+    { id: 2, name: 'AMZN', price: 3222 },
+    { id: 3, name: 'GOOG', price: 2800 },
+    { id: 4, name: 'IBM', price: 134 },
+]
